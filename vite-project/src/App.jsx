cdb7a2cc593cb4400860b11464ca4ce611fbb0ba
sushantsubedi35 <sushantsubedi352@gmail.com>
2026-02-04 16:02:27 +0545

    your message

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,7 @@ import Register from "./components/register/Register";
 function App(){
   return(
     <div>
- <p> hello guys</p>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+
 
     <Register/>
 
